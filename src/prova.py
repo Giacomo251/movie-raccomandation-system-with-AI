2bc7c2aa97fa295e38072892cdbcb765f9801443
Giacomo251 <giacomociardo@gmail.com>
2025-11-01 18:09:33 +0200

    commit

--- src/prova.py
+++
@@ -1,9 +0,0 @@
-from preprocessing import preprocess
-import pandas as pd
-
-df = pd.read_csv("data/imdb_movies.csv")
-X_train, X_val, X_test, y_train, y_val, y_test, scaler, mlb = preprocess(df)
-
-print("Shape X_train:", X_train.shape)
-print("Example features:", X_train[0][:10])
-print("Example label:", y_train[0])
